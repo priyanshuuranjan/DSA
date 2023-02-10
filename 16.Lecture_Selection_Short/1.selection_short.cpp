0e344a8583598f0011939c09919cea9f8a6a1f19
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-02-10 17:14:32 +0530

    Initial commit

--- 16.Lecture_Selection_Short/1.selection_short.cpp
+++ 16.Lecture_Selection_Short/1.selection_short.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 using namespace std;
+int getSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < minIndex)
+            {
+                minIndex = j;
+            }
+        }
+        swap(arr[minIndex], arr[i]);
+    }
+}
+// function to print an array
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
 int main()
 {
-    int n;
-    cout << "Enter the value of n" << endl;
-    cin >> n;
+    int arr[] = {5, 4, 10, 1, 6, 2};
+    int i, j, n, temp;
+    n = sizeof(arr) / sizeof(int);
+    cout << "Unsorted Array :";
+    print(arr, n);
+    getSort(arr, n);
+    cout << "Sorted Array :";
+    print(arr, n);
+    return (0);
 }
