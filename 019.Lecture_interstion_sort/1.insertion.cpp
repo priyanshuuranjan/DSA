f16cb9bb1328a863c85dd0a50406371b2800afd9
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-02-18 22:21:45 +0530

    add

--- 019.Lecture_interstion_sort/1.insertion.cpp
+++ 019.Lecture_interstion_sort/1.insertion.cpp
@@ -1,8 +1,53 @@
-#include <iostream>
+// C++ program for insertion sort
+
+#include <bits/stdc++.h>
 using namespace std;
+
+// Function to sort an array using
+// insertion sort
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int temp = arr[i];
+        int j;
+        for (j = i - 1; j >= 0; j--)
+        {
+
+            if (arr[j] > temp)
+            {
+                // shift
+                arr[j + 1] = arr[j];
+            }
+            else
+            {
+                // ruk jao
+                break;
+            } 
+        }
+        arr[j + 1] = temp;
+    }
+}
+
+// A utility function to print an array
+// of size n
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+// Driver code
 int main()
 {
-    int n;
-    cout << "Enter the value of n" << endl;
-    cin >> n;
+    int arr[] = {12, 11, 13, 5, 6};
+    int N = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, N);
+    printArray(arr, N);
+
+    return 0;
 }
+// This is code is contributed by rathbhupendra
