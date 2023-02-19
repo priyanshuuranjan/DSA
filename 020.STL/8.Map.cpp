3f67a954a2a31f6cc3e9953d3420be49af5c5d97
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-02-19 18:29:41 +0530

    Initial commit

--- 020.STL/8.Map.cpp
+++ 020.STL/8.Map.cpp
@@ -1,9 +1,20 @@
+// ishme ek key ek hi value ko point krta hai
 #include <iostream>
+#include <map>
 
 using namespace std;
 int main()
 {
-    int n;
-    cout << "Enter the value of n" << endl;
-    cin >> n;
+    map<int, string> m;
+
+    m[1] = "priyanshu";
+    m[13] = "priyanshu";
+    m[2] = "priyanshu";
+
+    m.insert({5, "alpha"});
+    cout << "before erase " << endl;
+    for (auto i : m)
+    {
+        cout << i.first << " " << i.second << endl;
+    }
 }
