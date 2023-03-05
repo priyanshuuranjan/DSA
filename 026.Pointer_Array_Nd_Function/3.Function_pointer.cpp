1392ad5e1b391303a7e4af244c153a3bc9c078b3
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-03-05 22:38:51 +0530

    push

--- 026.Pointer_Array_Nd_Function/3.Function_pointer.cpp
+++ 026.Pointer_Array_Nd_Function/3.Function_pointer.cpp
@@ -39,7 +39,7 @@ int main() {
 
     int arr[6] = {1,2,3,4,5,8};
 
-    cout << "Sum is " << getSum(arr+3 ,3) << endl ;  
+    cout << "Sum is " << getSum(arr+3 ,3) << endl ;  //sum 
 
     return 0;
 }
