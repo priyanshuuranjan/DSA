44aeab56fa7e474a50e85341e53167fd12bfe6a2
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-07-12 22:06:41 +0530

    update

--- 022.Char_Array_Question/1.Char.cpp
+++ 022.Char_Array_Question/1.Char.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int getLength(char name[])
 {
     int count = 0;
-    for (int i = 0; name[i] != '\0'; i++)
+    for (int i = 0; name[i] != '\0'; i++) // name[i] != '\0'
     {
         count++;
     }
