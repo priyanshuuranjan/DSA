b2f9b92e9d1f416701b53f3ddb3c5d4f349e8c30
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-07-13 22:18:10 +0530

    update

--- 023.2D-Array/1.Intro.cpp
+++ 023.2D-Array/1.Intro.cpp
@@ -3,37 +3,37 @@ using namespace std;
 
 int main()
 {
-    // Creating 2 d Array
 
-    int arr[3][4];
+    // create 2 d array
+    int arr[3][3];
 
-    // taking input -> row wise input.
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            cin >> arr[i][j];
-        }
-    }
-
-    // takig input ->> col wise input
-    // for (int j = 0; j < 4; j++)
-    // {
-    //     for (int i = 0; i < 3; i++)
-    //     {
-    //         cin >> arr[j][i];
-    //     }
-    // }
-
-    // printing part
+    cout << "Enter the elements " << endl;
+    // taking input -> row wise input
     for (int row = 0; row < 3; row++)
     {
-        for (int col = 0; col < 4; col++)
+        for (int col = 0; col < 3; col++)
+        {
+            cin >> arr[row][col];
+        }
+    }
+    /*
+    taking input -> col wise input
+        for(int col=0; col<4; col++) {
+            for(int row=0; row<3; row++) {
+                cin >> arr[row][col];
+            }
+        }
+
+    */
+
+    cout << "Printing the array " << endl;
+    // print
+    for (int row = 0; row < 3; row++)
+    {
+        for (int col = 0; col < 3; col++)
         {
             cout << arr[row][col] << " ";
         }
         cout << endl;
-
-        return 0;
     }
 }
