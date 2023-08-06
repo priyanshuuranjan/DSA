85c9f7a65abb4648424e00f826c61f2403bbcdcb
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-08-06 20:34:15 +0530

    Adding

--- 026.Pointer_Array_Nd_Function/2.Char_array.cpp
+++ 026.Pointer_Array_Nd_Function/2.Char_array.cpp
@@ -6,7 +6,7 @@ int main()
     int arr[5] = {0, 1, 2, 3, 4}; // integer array
 
     char ch[6] = "abcde";
-
+    // here in case of arr cout prints its address value while in char cout works differently .. it print the entire data
     cout << arr << endl;
     cout << ch << endl;
 
