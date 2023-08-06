56f2f50aa06935e25d4d91271475d656c48423b9
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-08-06 20:38:10 +0530

    Adding

--- 026.Pointer_Array_Nd_Function/3.Function_pointer.cpp
+++ 026.Pointer_Array_Nd_Function/3.Function_pointer.cpp
@@ -1,45 +1,49 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void print(int *p) {
+void print(int *p)
+{
 
-    cout << *p << endl; 
-
+    cout << *p << endl;
 }
 
-void update(int *p) {
+void update(int *p)
+{
 
-   // p = p + 1;
-   //cout << "inside "<< p <<endl;
-   *p = *p + 1;
-
+    // p = p + 1;
+    // cout << "inside "<< p <<endl;
+    *p = *p + 1;
 }
 
-int getSum(int *arr, int n) {
+int getSum(int *arr, int n)
+{
 
-    cout << endl << "Size : " << sizeof(arr) << endl;
+    cout << endl
+         << "Size : " << sizeof(arr) << endl;
 
     int sum = 0;
-    for(int i=0;i<n;i++) {
+    for (int i = 0; i < n; i++)
+    {
         sum += arr[i];
     }
     return sum;
 }
 
-int main() {
-/*
-    int value = 5;
-    int *p = &value;
+int main()
+{
+    /*
+        int value = 5;
+        int *p = &value;
 
-    //print(p);
-    cout <<" Before " << *p << endl;
-    update(p);
-    cout <<" After " << *p << endl;
-    */
+        //print(p);
+        cout <<" Before " << *p << endl;
+        update(p);
+        cout <<" After " << *p << endl;
+        */
 
-    int arr[6] = {1,2,3,4,5,8};
+    int arr[6] = {1, 2, 3, 4, 5, 8};
 
-    cout << "Sum is " << getSum(arr+3 ,3) << endl ;  // checking  the sum here
+    cout << "Sum is " << getSum(arr + 3, 3) << endl; // checking  the sum here
 
     return 0;
 }
