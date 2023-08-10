47964c9ccb940a6905fdbef1ebc6ae13ad635bcf
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-08-11 00:53:11 +0530

    Adding

--- 030.Global_Variable/inlineFunction.cpp
+++ 030.Global_Variable/inlineFunction.cpp
@@ -1,27 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-
-inline int getMax(int& a, int& b) {
-    return (a>b) ? a : b;
+inline int getMax(int &a, int &b)
+{
+    return (a > b) ? a : b;
 }
 
-int main() {
+int main()
+{
 
     int a = 1, b = 2;
-    int ans = 0; 
+    int ans = 0;
 
-    ans = getMax(a,b);
+    ans = getMax(a, b);
     cout << ans << endl;
 
     a = a + 3;
     b = b + 1;
 
-    ans = getMax(a,b);
+    ans = getMax(a, b);
     cout << ans << endl;
-
-
 
     return 0;
 }
