bb1d027dd7ca1491703ca08bc37d514030a9b151
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-08-11 00:55:15 +0530

    Adding

--- 030.Global_Variable/defaultArray.cpp
+++ 030.Global_Variable/defaultArray.cpp
@@ -1,23 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void print(int arr[], int n  , int start = 0) {
+void print(int arr[], int n, int start = 0)
+{
 
-    for(int i = start; i<n; i++) {
+    for (int i = start; i < n; i++)
+    {
         cout << arr[i] << endl;
     }
-
 }
 
-int main() {
+int main()
+{
 
-    int arr[5] = {1,4,7,8,9};
+    int arr[5] = {1, 4, 7, 8, 9};
     int size = 5;
 
     print(arr, size);
     cout << endl;
     print(arr, size, 2);
-
 
     return 0;
 }
