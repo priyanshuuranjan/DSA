5f48185b3ad916e3902febfd6bd60107e7d311a2
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-08-11 17:12:52 +0530

    Adding

--- 031.Recursion/Factorial.cpp
+++ 031.Recursion/Factorial.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int factorial(int n){
-    if (n == 0){
+int factorial(int n)
+{
+    //base case -->> Ruk jao
+    if (n == 0)
+    {
         return 1;
     }
 
-    int chhoti = factorial(n-1);
-    int badi = n*chhoti;
+    int chhoti = factorial(n - 1);
+    int badi = n * chhoti;
 
     return badi;
-
 }
-int main(){
+int main()
+{
 
     int n;
-    cin>>n;
-    cout<<factorial(n)<<endl;
+    cin >> n;
+    cout << factorial(n) << endl;
     return 0;
 }
