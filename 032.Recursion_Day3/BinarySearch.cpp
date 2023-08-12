3b15fea61e8baf3070d822486489f806c384137b
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-08-13 00:42:53 +0530

    Adding Codes

--- 032.Recursion_Day3/BinarySearch.cpp
+++ 032.Recursion_Day3/BinarySearch.cpp
@@ -3,12 +3,18 @@ using namespace std;
 int binarySearch(int arr[], int start, int end, int key)
 {
     // base case
+
+    // elememt not found
     if (start <= end)
     {
         return false;
     }
     int mid = start + (start - end) / 2;
-
+    // element found
+    if (arr[mid] == key)
+    {
+        return true;
+    }
     if ((arr[mid]) < key)
     {
         binarySearch(arr, mid + 1, end, key);
@@ -22,7 +28,9 @@ int binarySearch(int arr[], int start, int end, int key)
 
 int main()
 {
-    int n;
-    cout << "Enter the value of n" << endl;
-    cin >> n;
+    int arr[5] = {1, 5, 6, 7, 10};
+    int size = 5;
+    int key = 10;
+    cout << "found" << binarySearch(arr, size, key) << " " << endl;
+    return 0;
 }
