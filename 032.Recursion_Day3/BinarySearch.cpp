8d3ec6550154a62b50486c8d4277cd23bff4d702
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-08-13 00:49:18 +0530

    Adding Codes

--- 032.Recursion_Day3/BinarySearch.cpp
+++ 032.Recursion_Day3/BinarySearch.cpp
@@ -1,36 +1,49 @@
 #include <iostream>
 using namespace std;
-int binarySearch(int arr[], int start, int end, int key)
+
+void print(int arr[], int s, int e)
 {
+
+    for (int i = s; i <= e; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+bool binarySearch(int *arr, int s, int e, int k)
+{
+
     // base case
 
-    // elememt not found
-    if (start <= end)
-    {
+    // element not found
+    if (s > e)
         return false;
-    }
-    int mid = start + (start - end) / 2;
+
+    int mid = s + (e - s) / 2;
+
     // element found
-    if (arr[mid] == key)
-    {
+    if (arr[mid] == k)
         return true;
-    }
-    if ((arr[mid]) < key)
+
+    if (arr[mid] < k)
     {
-        binarySearch(arr, mid + 1, end, key);
+        return binarySearch(arr, mid + 1, e, k);
     }
     else
     {
-        binarySearch(arr, start, mid - 1, key);
+        return binarySearch(arr, s, mid - 1, k);
     }
-    mid = start + (start - end) / 2;
 }
 
 int main()
 {
-    int arr[5] = {1, 5, 6, 7, 10};
-    int size = 5;
-    int key = 10;
-    cout << "found" << binarySearch(arr, 0, 5, key) << " " << endl;
+
+    int arr[11] = {2, 4, 6, 10, 14, 18, 22, 38, 49, 55, 222};
+    int size = 11;
+    int key = 222;
+
+    cout << "Present or not: " << binarySearch(arr, 0, size - 1, key) << endl;
+
     return 0;
 }
