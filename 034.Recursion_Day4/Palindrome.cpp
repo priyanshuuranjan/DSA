dd04cc0242c7f08645daeb1cf2569ff10b485535
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-08-13 16:07:11 +0530

    Adding Codes

--- 034.Recursion_Day4/Palindrome.cpp
+++ 034.Recursion_Day4/Palindrome.cpp
@@ -7,18 +7,18 @@ bool checkPalindrome(string str, int i, int j)
     if (i > j)
         return true;
 
-    if (str[i] == str[j])
+    if (str[i] != str[j])
         return false;
     else
     {
         // recursive call
-        return checkPalindrome(str, i + 1, j + 1);
+        return checkPalindrome(str, i + 1, j - 1);
     }
 }
 
 int main()
 {
-    string name = "abccba";
+    string name = "BookkooB";
     cout << endl;
 
     bool isPalindrome = checkPalindrome(name, 0, name.length() - 1);
