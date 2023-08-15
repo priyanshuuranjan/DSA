d69d6abcf6aa8dd396c52c7656ea3058d9234ffc
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-08-15 16:19:32 +0530

    Adding Codes

--- 036.Recursion_Day6/QuickSort.cpp
+++ 036.Recursion_Day6/QuickSort.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 using namespace std;
+ int quickSort(int arr[], int s, int e){
+    //base case 
+    if (s >=e) return ;
+    
 
+ }
 
 int main()
 {
     int arr[15] = {3, 7, 0, 1, 5, 8, 3, 2, 34, 66, 87, 23, 12, 12, 12};
     int n = 15;
 
-    mergeSort(arr, 0, n - 1);
+    quickSort(arr, 0, n - 1);
 
     for (int i = 0; i < n; i++)
     {
