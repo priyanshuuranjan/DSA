0380e5d6e60c036df0f888cb84ccb1f19827f796
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-08-15 16:18:28 +0530

    Adding Codes

--- 036.Recursion_Day6/QuickSort.cpp
+++ 036.Recursion_Day6/QuickSort.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 using namespace std;
 
+
 int main()
 {
-    int n;
-    cout << "Enter the value of n" << endl;
-    cin >> n;
+    int arr[15] = {3, 7, 0, 1, 5, 8, 3, 2, 34, 66, 87, 23, 12, 12, 12};
+    int n = 15;
+
+    mergeSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
 }
 
