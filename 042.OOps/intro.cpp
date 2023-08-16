877dbfa1dfc2a9ea2d05cd40004ecbc583dd5683
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-08-16 17:23:09 +0530

    Adding Codes

--- 042.OOps/intro.cpp
+++ 042.OOps/intro.cpp
@@ -7,8 +7,9 @@ class Heroo
     // properties
 public:
     int health; // health
-    void print(){
-        cout<<"ye print hoga agr Heroo private v ho to kyo ki ye same clss k andar hi hai "<<endl;
+    void print()
+    {
+        cout << "ye print hoga agr Heroo private v ho to kyo ki ye same clss k andar hi hai " << endl;
     }
 };
 
@@ -17,10 +18,10 @@ int main()
     // creating object
     Heroo h1;
     Heroo ram;
-    cout << "health is:" << ram.health << endl;
+    cout << "health is: " << ram.health << endl;
 
     Hero alpha;
-    cout << "village is:" << alpha.village << endl;
+    cout << "village is: " << alpha.village << endl;
 
     cout << "size: " << sizeof(h1) << endl; // its rint 4
     return 0;
