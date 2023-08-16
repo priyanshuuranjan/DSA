61e16de6939e90fd78b626378a8eb2debf357354
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-08-16 17:33:04 +0530

    Adding Codes

--- 042.OOps/Hero.cpp
+++ 042.OOps/Hero.cpp
@@ -1,7 +1,7 @@
-class Hero
-{
-    // properties
-    public:
-    char level;
-    int village;
-};
+// class Hero
+// {
+//     // properties
+//     public:
+//     char level;
+//     char village;
+// };
--- 042.OOps/intro.cpp
+++ 042.OOps/intro.cpp
@@ -2,11 +2,12 @@
 #include "Hero.cpp"
 using namespace std;
 
-class Heroo
+class Hero
 {
     // properties
 public:
-    int health; // health
+    int health; 
+    char village;
     void print()
     {
         cout << "ye print hoga agr Heroo private v ho to kyo ki ye same clss k andar hi hai " << endl;
@@ -16,13 +17,17 @@ public:
 int main()
 {
     // creating object
-    Heroo h1;
-    Heroo ram;
-    cout << "health is: " << ram.health << endl;
-
+    // Hero h1;
     Hero alpha;
+    // abhi garbage value aarha hai ishko print krane pe ushke liye hm isko value hi dede rhe hai
+
+    alpha.health = 70;
+    alpha.village = "Sapahi";
+
+    cout << "health is: " << alpha.health << endl;
+
     cout << "village is: " << alpha.village << endl;
 
-    cout << "size: " << sizeof(h1) << endl; // its rint 4
+    // cout << "size: " << sizeof(h1) << endl; // its rint 4
     return 0;
 }
