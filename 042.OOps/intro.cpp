6f25a14d2cc83d1c103a96b180bc4032809fce5f
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-08-16 16:58:11 +0530

    Adding Codes

--- 042.OOps/intro.cpp
+++ 042.OOps/intro.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class Hero
+{
+    // properties
+    int health; // health
+};
+
 int main()
 {
-    int n;
-    cout << "Enter the value of n" << endl;
-    cin >> n;
+    // creating object
+    Hero h1;
+    cout << "size: " << sizeof(h1) << endl;
+    return 0;
 }
- 
