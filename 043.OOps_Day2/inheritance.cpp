58a822ec70eb25c9f3b2e14de3463ece6c7f9ff5
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-08-16 19:24:37 +0530

    Adding Codes

--- 043.OOps_Day2/inheritance.cpp
+++ 043.OOps_Day2/inheritance.cpp
@@ -45,18 +45,16 @@ int main()
     Male m1;
     // cout << m1.height << endl;
 
-    /*
-        Male object1;
-        cout << object1.age << endl;
-        cout << object1.weight << endl;
-        cout << object1.height << endl;
+    // Male object1;
+    // cout << object1.age << endl;
+    // cout << object1.weight << endl;
+    // cout << object1.height << endl;
 
-        cout << object1.color << endl;
+    // cout << object1.color << endl;
 
-        object1.setWeight(84);
-        cout << object1.weight << endl;
-        object1.sleep();
-        */
+    // object1.setWeight(84);
+    // cout << object1.weight << endl;
+    // object1.sleep();
 
     return 0;
 }
