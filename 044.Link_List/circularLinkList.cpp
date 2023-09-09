cc17d52748bc78fc8c6cd8fc11bc0bea7013bb63
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-09-09 23:33:18 +0530

    updating codes

--- 044.Link_List/circularLinkList.cpp
+++ 044.Link_List/circularLinkList.cpp
@@ -6,7 +6,6 @@ class Node
 public:
     int data;
     Node *next;
-
     // constrcutor
     Node(int d)
     {
@@ -98,7 +97,7 @@ void deleteNode(Node *&tail, int value)
             prev = curr;
             curr = curr->next;
         }
- 
+
         prev->next = curr->next;
 
         // 1 Node Linked List
