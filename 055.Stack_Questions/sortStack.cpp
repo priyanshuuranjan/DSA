4751fe3ff21eda2ef8f2a46ad6d4249037cb9865
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-09-20 22:48:36 +0530

    Stack Code Update

---
+++ 055.Stack_Questions/sortStack.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+// Function to insert an element at the correct position in a sorted stack
+void insertSorted(stack<int>& s, int element) {
+    if (s.empty() || element > s.top()) {
+        s.push(element);
+        return;
+    }
+
+    int topElement = s.top();
+    s.pop();
+    insertSorted(s, element);
+    s.push(topElement);
+}
+
+// Function to sort a stack using recursion
+void sortStack(stack<int>& s) {
+    if (!s.empty()) {
+        int topElement = s.top();
+        s.pop();
+        sortStack(s);
+        insertSorted(s, topElement);
+    }
+}
+
+int main() {
+    stack<int> myStack;
+
+    // Push elements onto the stack
+    myStack.push(5);
+    myStack.push(2);
+    myStack.push(9);
+    myStack.push(1);
+    myStack.push(6);
+
+    cout << "Original Stack: ";
+    stack<int> tempStack = myStack;
+    while (!tempStack.empty()) {
+        cout << tempStack.top() << " ";
+        tempStack.pop();
+    }
+    cout << endl;
+
+    // Sort the stack using recursion
+    sortStack(myStack);
+
+    cout << "Sorted Stack: ";
+    while (!myStack.empty()) {
+        cout << myStack.top() << " ";
+        myStack.pop();
+    }
+    cout << endl;
+
+    return 0;
+}
