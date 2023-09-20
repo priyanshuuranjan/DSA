874f2b0fba738bb5eee3dcd9747bed4650ca0c2c
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-09-20 22:29:25 +0530

    Stack Code Update

--- 055.Stack_Questions/Reverse_stack.cpp
+++ 055.Stack_Questions/Reverse_stack.cpp
@@ -65,3 +65,36 @@ int main() {
 
     return 0;
 }
+
+// LeetCode question easy steps...
+
+/* void insertAtBottom(stack<int> &s, int element) {
+    //baseCase
+    if(s.empty()) {
+        s.push(element);
+      	return ;
+    }
+    
+    int num = s.top();
+    s.pop();
+    
+    //recursive call
+    insertAtBottom(s, element);
+    
+    s.push(num);
+}
+
+void reverseStack(stack<int> &stack) {
+  	//base case
+    if(stack.empty()) {
+        return ;
+    }
+    
+    int num = stack.top();
+    stack.pop();
+    
+    //recursive call
+    reverseStack(stack);
+    
+    insertAtBottom(stack,num);
+}*/
