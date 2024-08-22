75cbf10fdca19e519dbfbd80a155412dd6d353a9
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-08-22 21:35:22 +0530

    Reverse

---
+++ 056.Recape/2.Reverse.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+    for (int i = n; i >= 1; i--)
+    {
+        cout << i << " " << endl;
+    }
+    return 0;
+}
