06b4644aa78b8d383802eb856a6062be33f22bdd
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-08-22 22:43:25 +0530

    Sum of sqr of n natural number

---
+++ 056.Recape/07.SumSquare.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the value of n" << endl;
+    cin >> n;
+    int sum = 0;
+    for (int i = 1; i < n; i++)
+    {
+        sum = sum + i * i;
+    }
+    cout << sum << endl;
+}
