555460341155505a087422b2badd36991947d101
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-08-22 21:25:29 +0530

    A to Z {Recape}

---
+++ 056.Recape/aTOz.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (char alphabet = 'a'; alphabet <= 'z'; alphabet++)
+    {
+        cout << alphabet << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
