6d3904fb009f36e51e33c4a79f0f8cd09bed7cf2
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-08-24 16:51:28 +0530

    P3

---
+++ 056.Recape/13.Pattern.cpp
@@ -0,0 +1,23 @@
+/*
+
+12345
+12345
+12345
+12345
+12345
+
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int row = 1; row <= 5; row++)
+    {
+        for (int col = 1; col <= 5; col++)
+        {
+            cout << col << "";
+        }
+        cout << endl;
+    }
+}
