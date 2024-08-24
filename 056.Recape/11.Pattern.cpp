6f1313628171e57dc2ba5bcc3ad502967bb4af71
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-08-24 14:54:00 +0530

    Upgrade Pattern

--- 056.Recape/11.Pattern.cpp
+++ 056.Recape/11.Pattern.cpp
@@ -1,14 +1,34 @@
+// 1st way 
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     for (int j = 1; j <= 5; j++)
+//     {
+//         for (int i = 1; i <= 5; i++)
+//         {
+//             cout << "*" << "";
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    for (int j = 1; j <= 5; j++)
+    for (int row = 1; row <= 5; row++)
     {
-        for (int i = 1; i <= 5; i++)
+        for (int col = 1; col <= 5; col++)
         {
             cout << "*" << "";
         }
         cout << endl;
     }
 }
+
