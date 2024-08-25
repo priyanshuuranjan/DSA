eaa147da3dd15036f6992145d4c04a23ac812b35
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-08-25 14:35:53 +0530

    P11

---
+++ 056.Recape/21.Pattern.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int row = 1; row <= 5; row++)
+    {
+        for (int col = 1; col <= row; col++)
+        {
+            cout <<  row << " ";
+
+        }
+        cout << endl;
+    }
+}
+
+/*
+
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5 
+
+*/
