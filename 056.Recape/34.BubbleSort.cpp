58b343d9c962c6fa90b9e1725e27356efc350fd2
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-09-05 15:34:57 +0530

    Bubble Sort

---
+++ 056.Recape/34.BubbleSort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //! Ye phle wala code for printing and taking the input from the user k liye hai
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                Swap arr[j] and arr[j+1]
+                // int temp = arr[j];
+                // arr[j] = arr[j + 1];
+                // arr[j + 1] = temp;
+            }
+        }
+    }
+
+    cout << "Array after sorting: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
