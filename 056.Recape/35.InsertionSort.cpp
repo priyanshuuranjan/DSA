20dc59b9051a4558a43dad3c91559fbb98f0464c
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-09-05 15:54:52 +0530

    Insertion Sort

---
+++ 056.Recape/35.InsertionSort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[1000];
+    int n;
+    cout<<"Enter the size of the array";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 1; i < n ; i++)
+    {
+        for (int j = i ; j > 0; j--)
+        {
+
+           if (arr[j] < arr[j - 1])
+            {
+                swap(arr[j], arr[j - 1]);
+                break;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
