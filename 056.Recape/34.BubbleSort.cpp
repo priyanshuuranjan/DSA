4d0eceb35a520f5a739c0d928d67d99801add63e
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-09-05 15:40:49 +0530

    Bubble Sort

--- 056.Recape/34.BubbleSort.cpp
+++ 056.Recape/34.BubbleSort.cpp
@@ -21,7 +21,7 @@ int main()
         {
             if (arr[j] > arr[j + 1])
             {
-                Swap arr[j] and arr[j+1]
+                swap(arr[j], arr[j + 1])
                 // int temp = arr[j];
                 // arr[j] = arr[j + 1];
                 // arr[j + 1] = temp;
