ce04acd06aca038a2556334c9afda4be73373eaa
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-09-05 15:03:24 +0530

    Selection Sort updates

--- 056.Recape/33.Selection.cpp
+++ 056.Recape/33.Selection.cpp
@@ -3,12 +3,18 @@ using namespace std;
 
 int main()
 {
-    int arr[6] = {10, 4, 1, 3, 2, 7};
+    int arr[1000];
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < n - 1; i++)
     {
         int index = i;
-        for (int j = i + 1; j < 6; j++)
+        for (int j = i + 1; j < n; j++)
         {
 
             if (arr[j] < arr[index])
@@ -19,7 +25,7 @@ int main()
         swap(arr[i], arr[index]);
     }
 
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
