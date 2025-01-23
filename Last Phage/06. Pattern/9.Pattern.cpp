b02b6799dc26c4e75d4f6a62c401f8ae5c74a399
Priyanshu Rajan <priyanshumth0808@gmail.com>
2025-01-24 00:21:45 +0530

    add: pattern problem

---
+++ Last Phage/06. Pattern/9.Pattern.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int row = 1; row <= 5; row++)
+    {
+        for (int col = 1; col <= row; col++)
+        {
+            cout << col << " ";
+        }
+        cout << endl;
+    }
+}
+
+/*
+
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+*/
